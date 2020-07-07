40b5fce30d000cf56c2ce7bd02100150286f6850
Latimore <Justin.Latimore3@T-Mobile.com>
2020-07-07 03:29:33 -0400

    First Commit

---
+++ indexOG.js
@@ -0,0 +1,49 @@
+/// Required Webdriver Controller ///
+const webdriver = require('selenium-webdriver'),
+  By = webdriver.By,
+  until = webdriver.until;
+
+/// Create a driver for your required testing browser (IE,Edge, Chrome,FireFox, Safari) ///
+const driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .build();
+
+/// Tester Email for Firewall access ///
+const tester = "justin.latimore3@t-mobile.com";
+
+/// The URL to the desired testing ///
+driver.get('https://tmobileusa.sharepoint.com/sites/intranet');
+
+/// Login through T-Mobile Firewall ///
+driver.sleep(10000).then(function () {
+  driver.findElement(By.name('loginfmt')).sendKeys(tester);
+  driver.findElement(By.name('q')).sendKeys(webdriver.Key.TAB);
+  driver.findElement(By.id('idSIButton9')).click();
+});
+
+/// Example Function 1///
+driver.sleep(20000).then( function () {
+  driver.getTitle().then( function (title) {
+    if (title === 'T-Nation Home') {
+      console.log('Test passed');
+    } else {
+      console.log('Test failed');
+    }
+    driver.quit();
+  });
+});
+
+/// Example Function 2///
+driver.sleep(20000).then( function () {
+  driver.getTitle().then( function (title) {
+    describe('#HomePage Check', function () {
+      it('should see the T-Nation Home page === title', function (done) {
+        title === 'T-Nation Home';
+        done();
+      });
+    });
+
+/// Kills Script ///
+    driver.quit();
+  });
+});
---
+++ testTemplate.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+driver.sleep(2000).then(function() {
+    driver.getTitle().then(function(title) {
+      if(title === 'webdriver - Google Search') {
+        console.log('Test passed');
+      } else {
+        console.log('Test failed');
+      }
+    });
+  });
